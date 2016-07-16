31c9cfe298920b100514cba0d6149f62b3b42569
Doctorintro <doctorintro@gmail.com>
2016-07-16 16:09:50 +0200

    Delayd Invenory open

--- FirstRound/src/main/java/de/doctorintro/wizardbrawl/listener/InventoryCloseListener.java
+++ FirstRound/src/main/java/de/doctorintro/wizardbrawl/listener/InventoryCloseListener.java
@@ -1,10 +1,12 @@
 package de.doctorintro.wizardbrawl.listener;
 
 import de.doctorintro.wizardbrawl.WizardBrawl;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 
 /**
  * Created by Doctorintro on 16.07.2016.
@@ -18,11 +20,15 @@ public class InventoryCloseListener implements Listener{
     }
 
     @EventHandler
-    public void onClose(InventoryCloseEvent e){
-        Player p  = (Player) e.getPlayer();
-        if (plugin.getKitManager().getKit(p) == null) {
-            p.openInventory(plugin.getInventoryManager().getSelectWizard());
+    public void onClose(InventoryCloseEvent e) {
+        Player p = (Player) e.getPlayer();
+        Inventory inv = e.getInventory();
+        if (inv.getTitle().equals(plugin.getInventoryManager().getSelectWizard().getTitle())) {
+            if (plugin.getKitManager().getKit(p) == null) {
+                Bukkit.getScheduler().runTaskLater(plugin, ()->{
+                    p.openInventory(plugin.getInventoryManager().getSelectWizard());
+                }, 1);
+            }
         }
     }
-
 }
