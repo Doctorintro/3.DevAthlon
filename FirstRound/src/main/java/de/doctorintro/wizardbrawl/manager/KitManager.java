ab42441d51e1e96a7692897d12b68c1b69e175cb
Doctorintro <doctorintro@gmail.com>
2016-07-16 20:07:16 +0200

    Armor now Unbreakable

--- FirstRound/src/main/java/de/doctorintro/wizardbrawl/manager/KitManager.java
+++ FirstRound/src/main/java/de/doctorintro/wizardbrawl/manager/KitManager.java
@@ -4,6 +4,7 @@ import de.doctorintro.wizardbrawl.WizardBrawl;
 import de.doctorintro.wizardbrawl.kit.IKit;
 import de.doctorintro.wizardbrawl.kit.Kit;
 import de.doctorintro.wizardbrawl.spells.BasicTestSpell;
+import de.doctorintro.wizardbrawl.utils.ItemFactory;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -19,10 +20,12 @@ public class KitManager {
 
     private WizardBrawl plugin;
     private List<IKit> kits;
+    private Armor armor;
 
     public KitManager(WizardBrawl plugin) {
         this.plugin = plugin;
         kits = new LinkedList<>();
+        armor = new Armor();
         loadKits();
     }
 
@@ -45,10 +48,32 @@ public class KitManager {
         IKit k = getKit(name);
         if (k == null) return;
         p.getInventory().clear();
-        p.getInventory().setArmorContents(new ItemStack[]{new ItemStack(Material.LEATHER_BOOTS), new ItemStack(Material.LEATHER_LEGGINGS), new ItemStack(Material.LEATHER_CHESTPLATE), k.getIcon()});
+        p.getInventory().setArmorContents(new ItemStack[]{armor.getBoots(), armor.getLeggins(), armor.getChestplate(), k.getIcon()});
         p.getInventory().setItemInOffHand(null);
         p.getInventory().setItem(0, k.getActiv().getItemStack());
         p.getInventory().setItem(1, k.getPassiv().getItemStack());
         plugin.getPlayerManager().onKitChoose(p, k);
+    }
+
+    private class Armor{
+        private ItemStack boots, leggins, chestplate;
+
+        public Armor() {
+            boots = new ItemFactory(new ItemStack(Material.LEATHER_BOOTS)).setUnbreakable().build();
+            leggins = new ItemFactory(new ItemStack(Material.LEATHER_LEGGINGS)).setUnbreakable().build();
+            chestplate = new ItemFactory(new ItemStack(Material.LEATHER_CHESTPLATE)).setUnbreakable().build();
+        }
+
+        public ItemStack getBoots() {
+            return boots;
+        }
+
+        public ItemStack getLeggins() {
+            return leggins;
+        }
+
+        public ItemStack getChestplate() {
+            return chestplate;
+        }
     }
 }
