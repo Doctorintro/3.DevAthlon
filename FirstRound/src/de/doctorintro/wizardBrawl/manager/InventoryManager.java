ba106debc255d73156b79eb9432069712dc6ee47
Doctorintro <doctorintro@gmail.com>
2016-07-16 14:11:31 +0200

    Adding KitChoosInventorys

--- FirstRound/src/de/doctorintro/wizardBrawl/manager/InventoryManager.java
+++ FirstRound/src/de/doctorintro/wizardBrawl/manager/InventoryManager.java
@@ -1,8 +1,15 @@
 package de.doctorintro.wizardBrawl.manager;
 
 import de.doctorintro.wizardBrawl.WizardBrawl;
+import de.doctorintro.wizardBrawl.kit.IKit;
+import de.doctorintro.wizardBrawl.utils.ItemFactory;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by Doctorintro on 16.07.2016.
@@ -12,16 +19,34 @@ public class InventoryManager {
     public WizardBrawl plugin;
 
     public Inventory selectWizard;
+    public Map<String, Inventory> kits;
 
     public InventoryManager(WizardBrawl plugin) {
         this.plugin = plugin;
+        this.kits = new LinkedHashMap<>();
 
         createSelectWizard();
+        loadKitInventorys();
     }
 
-    /**
-     * TODO: Create Inventorys
-     **/
+    private void loadKitInventorys() {
+        for(IKit kit : plugin.getKitManager().getKits()){
+            Inventory add = Bukkit.createInventory(null , 3*9, "Zunft des "+kit.getName());
+            for (int i = 0; i < add.getSize(); i++)
+                add.setItem(i, new ItemFactory( new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 15) ).setDisplayName("").build());
+            for(int i = 0; i < add.getSize()-9; i+= 9)
+                add.setItem(0, new ItemFactory( new ItemStack(Material.WOOL, 1, (short) 5) ).setDisplayName("§aAuswählen").build());
+            for(int i = 8; i < add.getSize()-9; i+= 9)
+                add.setItem(8, new ItemFactory( new ItemStack(Material.WOOL, 1 , (short)14) ).setDisplayName("§cAbbrechen").build());
+            add.setItem(13, kit.getIcon());
+            add.setItem(20, kit.getActiv());
+            add.setItem(14, kit.getPassiv());
+        }
+    }
+
+    public Inventory getKitInventor(String kitName){
+        return kits.get(kitName);
+    }
 
     private void createSelectWizard(){
         int kits = plugin.getKitManager().getKits().size();
@@ -29,7 +54,7 @@ public class InventoryManager {
         while ( kits > size && size < 54){
             size += 9;
         }
-        selectWizard = Bukkit.createInventory(null, size, "§e§lWähle dein Stamm");
+        selectWizard = Bukkit.createInventory(null, size, "§e§lWähle deine Zunft");
         for (int i = 0; i < plugin.getKitManager().getKits().size(); i++) {
             selectWizard.setItem(i, plugin.getKitManager().getKits().get(i).getIcon());
         }
