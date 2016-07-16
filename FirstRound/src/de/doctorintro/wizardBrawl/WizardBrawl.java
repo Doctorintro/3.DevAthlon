165ae65f9699119e0ecda75cbf17c1fe1f9c8eae
Doctorintro <doctorintro@gmail.com>
2016-07-16 15:12:35 +0200

    Register Listener

--- FirstRound/src/de/doctorintro/wizardBrawl/WizardBrawl.java
+++ FirstRound/src/de/doctorintro/wizardBrawl/WizardBrawl.java
@@ -1,8 +1,14 @@
 package de.doctorintro.wizardBrawl;
 
+import de.doctorintro.wizardBrawl.listener.InventoryClickListener;
+import de.doctorintro.wizardBrawl.listener.InventoryCloseListener;
+import de.doctorintro.wizardBrawl.listener.ListenerToCancel;
+import de.doctorintro.wizardBrawl.listener.PlayerJoinListener;
 import de.doctorintro.wizardBrawl.manager.InventoryManager;
 import de.doctorintro.wizardBrawl.manager.KitManager;
 import de.doctorintro.wizardBrawl.manager.LocationManager;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Random;
@@ -20,16 +26,21 @@ public class WizardBrawl extends JavaPlugin{
 
     public void onEnable() {
         loadManager();
+        registerListener();
+    }
+
+    private void registerListener() {
+        PluginManager manager = Bukkit.getPluginManager();
+        manager.registerEvents(new InventoryClickListener(this), this);
+        manager.registerEvents(new InventoryCloseListener(this), this);
+        manager.registerEvents(new ListenerToCancel(), this);
+        manager.registerEvents(new PlayerJoinListener(this), this);
     }
 
     private void loadManager() {
         locationManager = new LocationManager(this);
         kitManager = new KitManager();
         inventoryManager = new InventoryManager(this);
-    }
-
-    public void onDisable() {
-
     }
 
     public Random getRandom() {
