037f6a9d0b2624e92911cc27c8c4eb48f47aa12b
Doctorintro <doctorintro@gmail.com>
2016-07-16 12:35:51 +0200

    Adding selectWizard variable

---
+++ FirstRound/src/de/doctorintro/wizardBrawl/manager/InventoryManager.java
@@ -0,0 +1,27 @@
+package de.doctorintro.wizardBrawl.manager;
+
+import org.bukkit.inventory.Inventory;
+
+/**
+ * Created by Doctorintro on 16.07.2016.
+ */
+public class InventoryManager {
+
+    public Inventory selectWizard;
+
+    /**
+     * TODO: Create Inventorys
+     */
+
+    public InventoryManager() {
+
+    }
+
+    private void createSelectWizard(){
+
+    }
+
+    public Inventory getSelectWizard() {
+        return selectWizard;
+    }
+}
