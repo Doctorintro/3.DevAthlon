a79e66f0339edba396f1a3432ef6d65b86bea257
Doctorintro <doctorintro@gmail.com>
2016-07-23 11:40:35 +0200

    Shut down is not register

--- SecondRound/Proxy/src/de/doctorintro/proxy/ProxyPacketHandler.java
+++ SecondRound/Proxy/src/de/doctorintro/proxy/ProxyPacketHandler.java
@@ -1,8 +1,10 @@
 package de.doctorintro.proxy;
 
 import de.doctorintro.codec.Packet;
+import de.doctorintro.packets.ClientRegisterResultPacket;
 import de.doctorintro.utils.PacketHandler;
 import io.netty.channel.ChannelHandlerContext;
+import net.md_5.bungee.BungeeCord;
 
 /**
  * Created by Doctorintro on 23.07.2016.
@@ -11,6 +13,9 @@ public class ProxyPacketHandler extends PacketHandler {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Packet msg) throws Exception {
-
+        if (msg instanceof ClientRegisterResultPacket) {
+            ClientRegisterResultPacket packet = (ClientRegisterResultPacket) msg;
+            if (!packet.isRegisterd()) BungeeCord.getInstance().stop("Es ist bereits ein andere Proxy regiistriert.");
+        }
     }
 }
