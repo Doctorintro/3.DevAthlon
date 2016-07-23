69b896630f8c43426f562a8ab6637b6adc19f89d
Doctorintro <doctorintro@gmail.com>
2016-07-23 11:15:50 +0200

    Making the Proxyclient ready

---
+++ SecondRound/Proxy/src/de/doctorintro/proxy/ProxyMain.java
@@ -0,0 +1,18 @@
+package de.doctorintro.proxy;
+
+import de.doctorintro.packets.utils.Client;
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * Created by Doctorintro on 23.07.2016.
+ */
+public class ProxyMain extends Plugin {
+
+    private Client client;
+
+    @Override
+    public void onEnable() {
+        client = new Client("localhost", 12546, new ProxyPacketHandler());
+        client.run();
+    }
+}
---
+++ SecondRound/Proxy/src/de/doctorintro/proxy/ProxyPacketHandler.java
@@ -0,0 +1,16 @@
+package de.doctorintro.proxy;
+
+import de.doctorintro.codec.Packet;
+import de.doctorintro.packets.utils.PacketHandler;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created by Doctorintro on 23.07.2016.
+ */
+public class ProxyPacketHandler extends PacketHandler {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Packet msg) throws Exception {
+
+    }
+}
